7f5ddd0b7672209f984fba1887c463e342c023b7
Manfred <mac@MacBook-Pro-de-Mac.local>
2024-01-19 17:20:50 +0100

    create categories filters first part

--- scripts/functions/filterManagement.js
+++ scripts/functions/filterManagement.js
@@ -1,4 +1,30 @@
 export function filterManagement() {
-    const filters = document.querySelectorAll('.filters');
-    console.log(filters);
+    const arrowDown = document.querySelectorAll('.arrow-down');
+    arrowDown.forEach(arrow => {
+        arrow.addEventListener('click', () => {
+            const parentTitle = arrow.parentElement;
+            const parentCategory = parentTitle.parentElement;
+            parentCategory.classList.toggle('active');
+
+            if (parentCategory.classList.contains('active')) {
+                arrow.style.transform = 'rotate(calc(90deg - 180deg))';
+                let newDiv = document.createElement('div');
+                newDiv.classList.add('newDiv');
+                newDiv.innerHTML = `
+                    <input type="text" class="search-category" aria-label="Search category" ">
+                    <ul>
+                     <li>Pomme de terre</li>
+                     <li>Pomme de terre</li>
+                     <li>Pomme de terre</li>
+                    </ul>`;
+                parentCategory.appendChild(newDiv);
+            } else {
+                arrow.style.transform = 'rotate(calc(90deg - 0deg))';
+                let newDiv = parentCategory.querySelector('.newDiv');
+                if (newDiv) {
+                    parentCategory.removeChild(newDiv);
+                }
+            }
+        }) 
+    })
 }
